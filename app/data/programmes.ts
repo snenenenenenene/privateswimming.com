fd18267d27fc35ccc1735b8a12b4400d24d6c97b
snenenenenenene <sennebels@gmail.com>
2023-04-01 10:30:39 +0200

    feat: :sparkles: split off programmes

---
+++ app/data/programmes.ts
@@ -0,0 +1,20 @@
+export const programmes = [
+  {
+    id: 1,
+    title: "Starter",
+    price: 70,
+    image: "assets/programmes/plan-1.jpg",
+  },
+  {
+    id: 1,
+    title: "Starter",
+    price: 70,
+    image: "assets/programmes/plan-1.jpg",
+  },
+  {
+    id: 1,
+    title: "Starter",
+    price: 70,
+    image: "assets/programmes/plan-1.jpg",
+  },
+];
